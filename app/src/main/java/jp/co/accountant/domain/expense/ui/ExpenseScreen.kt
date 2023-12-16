b7b6c6ae32fe776b0036603d67903a6a538dccf8
akihiyo76 <ak.sato.info@gmail.com>
2023-12-17 01:47:36 +0900

    refactor ui

--- app/src/main/java/jp/co/accountant/domain/expense/ui/ExpenseScreen.kt
+++ app/src/main/java/jp/co/accountant/domain/expense/ui/ExpenseScreen.kt
@@ -1,112 +1,41 @@
 package jp.co.accountant.domain.expense.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.unit.dp
-import jp.co.accountant.domain.expense.ui.component.SearchDialog
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import jp.co.accountant.R
+import jp.co.accountant.app.ui.theme.AccountantAppTheme
+import jp.co.accountant.domain.expense.ui.component.DepartmentInput
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpenseScreen() {
-    var query by remember { mutableStateOf("") }
-    var showSearchDialog by remember { mutableStateOf(false) }
-    val focusManager = LocalFocusManager.current
-
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(title = { Text(text = "経費申請") })
         }
     ) { paddingValues ->
-        Surface(modifier = Modifier.padding(paddingValues)) {
-            Column {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    OutlinedTextField(
-                        value = query,
-                        onValueChange = { newValue ->
-                            query = newValue
-                        },
-                        modifier = Modifier
-                            .weight(1f)
-                            .height(IntrinsicSize.Min),
-                        trailingIcon = {
-                            if (query.isNotEmpty()) {
-                                IconButton(onClick = {
-                                    query = ""
-                                }) {
-                                    Icon(
-                                        imageVector = Icons.Default.Clear,
-                                        contentDescription = null
-                                    )
-                                }
-                            }
-                        }
-                    )
-                    Button(
-                        onClick = {
-                            showSearchDialog = true
-                            focusManager.clearFocus()
-                        },
-                        modifier = Modifier
-                            .height(IntrinsicSize.Min)
-                            .padding(start = 8.dp),
-                        shape = RoundedCornerShape(4.dp)
-                    ) {
-                        Text(
-                            text = "検索",
-                            style = MaterialTheme.typography.labelLarge
-                        )
-                    }
-                }
-            }
-
-            if (showSearchDialog) {
-                SearchDialog(
-                    text = query,
-                    onSelectDepartment = { department ->
-                        query = department.name
-                        showSearchDialog = false
-                    },
-                    onDismissRequest = {
-                        showSearchDialog = false
-                    }
-                )
-            }
+        Surface(
+            modifier = Modifier
+                .padding(top = paddingValues.calculateTopPadding())
+                .padding(horizontal = dimensionResource(id = R.dimen.large_space))
+        ) {
+            DepartmentInput()
         }
     }
 }
 
-// @Preview
-// @Composable
-// private fun PreviewExpenseContent() {
-//    AccountantAppTheme {
-//        ExpenseContent()
-//    }
-// }
+@Preview
+@Composable
+private fun PreviewExpenseScreen() {
+    AccountantAppTheme {
+        ExpenseScreen()
+    }
+}
---
+++ app/src/main/java/jp/co/accountant/domain/expense/ui/component/DepartmentInput.kt
@@ -0,0 +1,90 @@
+package jp.co.accountant.domain.expense.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DepartmentInput() {
+    var query by remember { mutableStateOf("") }
+    var showSearchDialog by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = query,
+                onValueChange = { newValue ->
+                    query = newValue
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .height(IntrinsicSize.Min),
+                trailingIcon = {
+                    if (query.isNotEmpty()) {
+                        IconButton(onClick = {
+                            query = ""
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Clear,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                }
+            )
+            Button(
+                onClick = {
+                    showSearchDialog = true
+                    focusManager.clearFocus()
+                },
+                modifier = Modifier
+                    .height(IntrinsicSize.Min)
+                    .padding(start = 8.dp),
+                shape = RoundedCornerShape(4.dp)
+            ) {
+                Text(
+                    text = "検索",
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+        }
+    }
+
+    if (showSearchDialog) {
+        SearchDialog(
+            text = query,
+            onSelectDepartment = { department ->
+                query = department.name
+                showSearchDialog = false
+            },
+            onDismissRequest = {
+                showSearchDialog = false
+            }
+        )
+    }
+}
